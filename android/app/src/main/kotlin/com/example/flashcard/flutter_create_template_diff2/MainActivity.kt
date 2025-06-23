b90c61b4aeb5be01569191851cfa6c3c4886ebc5
shoichi namba <alphabrend@gmail.com>
2025-06-23 16:52:45 +0900

    3.24.3

--- android/app/src/main/kotlin/com/example/flashcard/flutter_create_template_diff2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flashcard.flutter_create_template_diff2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/flutter_create_template_diff2/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_create_template_diff2/MainActivity.kt
@@ -1,4 +1,4 @@
-package flutter_create_template_diff2.flutter_create_template_diff2
+package com.example.flutter_create_template_diff2
 
 import io.flutter.embedding.android.FlutterActivity
 
