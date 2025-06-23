25f0d5b2423fd0ae3523246780f67b4710ca9573
shoichi namba <alphabrend@gmail.com>
2025-06-23 16:59:52 +0900

    3.32.0

--- android/app/src/main/kotlin/com/example/flutter_create_template_diff2/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_create_template_diff2/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.flutter_create_template_diff2
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
